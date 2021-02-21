b44a5bcb09854bbeed9d9a38b33126d0690e1a09
dpooria <dpooria75@gmail.com>
2021-02-21 17:06:32 +0330

    fix calender

--- calenderAPI.py
+++ calenderAPI.py
@@ -249,14 +249,14 @@ class Calender:
                         tr_single_date(date_list[0]))
                 return answer, generated_sentence
         if events:
-            j = jalali_to_gregorian(
+            j = gregorian_to_jalali(
                 date_list[0].year, date_list[0].month, date_list[0].day)
             answer["result"].append(format_jalali_date(j))
             generated_sentence = "{} مصادف با {} میباشد".format(
                 " ".join(events), tr_single_date(date_list[0]))
             return answer, generated_sentence
         if bii:
-            j = jalali_to_gregorian(
+            j = gregorian_to_jalali(
                 date_list[0].year, date_list[0].month, date_list[0].day)
             answer["result"].append(format_jalali_date(j))
             generated_sentence = "{}، {} است".format(
